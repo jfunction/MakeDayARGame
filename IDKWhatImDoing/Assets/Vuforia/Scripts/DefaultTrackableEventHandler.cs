43e914ff02ad12d988e636c6cb6be0e1014ca6eb
jfunction <jarednorman@hotmail.com>
2018-10-25 17:16:32 +0200

    Initial Commit

---
+++ IDKWhatImDoing/Assets/Vuforia/Scripts/DefaultTrackableEventHandler.cs
@@ -0,0 +1,217 @@
+/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Vuforia;
+
+/// <summary>
+/// A custom handler that implements the ITrackableEventHandler interface.
+///
+/// Changes made to this file could be overwritten when upgrading the Vuforia version.
+/// When implementing custom event handler behavior, consider inheriting from this class instead.
+/// </summary>
+public class DefaultTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    #region PROTECTED_MEMBER_VARIABLES
+    protected TrackableBehaviour mTrackableBehaviour;
+    protected TrackableBehaviour.Status m_PreviousStatus;
+    protected TrackableBehaviour.Status m_NewStatus;
+
+    #endregion // PROTECTED_MEMBER_VARIABLES
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+    public string api_key = "sandbox_MDFmNjdiMDYtYjA1MC00Y2MxLThmZTgtZmJkM2NhNTBhNTM0LjdYSURoeWg0cnp3QUtoVEZBWk5HNTRERC1UdDlGNjRa";
+    public TextMesh egg1, egg2;
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        m_PreviousStatus = previousStatus;
+        m_NewStatus = newStatus;
+
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NO_POSE)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    /* Create a Root Object to store the returned json data in */
+    [System.Serializable]
+    public class Quotes
+    {
+        public Quote[] values;
+    }
+
+    [System.Serializable]
+    public class Quote
+    {
+        public string package_name;
+        public string sum_assured;
+        public int base_premium;
+        public string suggested_premium;
+        public string created_at;
+        public string quote_package_id;
+        public QuoteModule module;
+    }
+
+    [System.Serializable]
+    public class QuoteModule
+    {
+        public string type;
+        public string make;
+        public string model;
+    }
+
+    [Serializable]
+    public class Param
+    {
+        public Param (string _key, string _value) {
+        key = _key;
+        value = _value;
+        }
+        public string key;
+        public string value;
+    }
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Enable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = true;
+
+        // Enable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = true;
+
+        // Enable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = true;
+        
+        switch(mTrackableBehaviour.TrackableName){
+            case "egg1":
+                CreateQuote("iPhone 6s 64GB LTE", egg1);
+                break;
+            case "egg2":
+                CreateQuote("iPhone 7 Plus 32GB LTE", egg2);
+                break;
+        }
+    }
+    public void CreateQuote(string gadget, TextMesh obj) {
+        List<Param> parameters = new List<Param>();
+        parameters.Add(new Param("type", "root_gadgets"));
+        parameters.Add(new Param("model_name", gadget));
+
+        StartCoroutine(CallAPICoroutine("https://sandbox.root.co.za/v1/insurance/quotes", parameters, obj));
+    }
+
+    IEnumerator CallAPICoroutine(String url, List<Param> parameters, TextMesh obj)
+    {
+        Debug.Log("Using API key "+api_key);
+        string auth = api_key + ":";
+        auth = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(auth));
+        auth = "Basic " + auth;
+
+        WWWForm form = new WWWForm();
+
+        foreach (var param in parameters) {
+            form.AddField(param.key, param.value);
+        }
+
+        UnityWebRequest www = UnityWebRequest.Post(url, form);
+        www.SetRequestHeader("AUTHORIZATION", auth);
+        yield return www.Send();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Quotes json = JsonUtility.FromJson<Quotes>("{\"values\":" + www.downloadHandler.text + "}");
+
+            String text = "Make: " + json.values[0].module.make + "\nPremium: R" + (json.values[0].base_premium / 100);
+            Debug.Log("Form upload complete!");
+            Debug.Log(text);
+            obj.text = text;
+        }
+        yield return true;
+    }
+
+
+    protected virtual void OnTrackingLost()
+    {
+        var rendererComponents = GetComponentsInChildren<Renderer>(true);
+        var colliderComponents = GetComponentsInChildren<Collider>(true);
+        var canvasComponents = GetComponentsInChildren<Canvas>(true);
+
+        // Disable rendering:
+        foreach (var component in rendererComponents)
+            component.enabled = false;
+
+        // Disable colliders:
+        foreach (var component in colliderComponents)
+            component.enabled = false;
+
+        // Disable canvas':
+        foreach (var component in canvasComponents)
+            component.enabled = false;
+    }
+
+    #endregion // PROTECTED_METHODS
+}
